4e087ab14e84015e8409ff5b9d12223e319607a5
Liwidis <694907182@qq.com>
2023-03-21 17:13:18 +0800

    增加了单调性、因子暴露的函数，以及long short收益矩阵

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,8 +5,20 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_columns', None)
 
 
+def winsorize_plot(data, vertical_lines=[]):
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(111)
+    _df = ax.hist(data, 100, density=True)
+    color_list = ['red', 'green', 'yellow', 'black', 'gold', 'gray']
+    count = 0
+    for value in vertical_lines:
+        ax.bar(value, 0.1, width=0.05, color=color_list[count], alpha=1)
+        count += 1
+    plt.show()
+
+
 def return_rate_matrix():
-    # 生成return矩阵
+    # 生成return_rate矩阵
     ret = pd.DataFrame((np.random.rand(20, 30) - 0.5) * 0.42)
     return ret
 
@@ -63,20 +71,26 @@ lamda:数字，决定做多做空多少比例的数字
 '''
 
 
-def computing_1(matrix: pd.DataFrame, dummy: pd.DataFrame, lamda):
+def computing_1(matrix: pd.DataFrame, dummy: pd.DataFrame, lamda, boxes):
     # 计算M1:对所有数字进行排序，nan未计入
     matrix = matrix[dummy]
-    rank_matrix = matrix.rank(axis=1)
+    rank_matrix = matrix.rank(axis=1, method='first')
 
     # 生成M2：找到每一行最大的数字
     max_numbers = rank_matrix.max(axis=1)
 
-    # 生成M3：对排位进行划分
+    # 生成M3：对排位进行boxes划分
+    boxes_list = []
+    for i in range(0, boxes):
+        box_1 = rank_matrix >= i / boxes * max_numbers
+        box_2 = rank_matrix < ((i+1) / boxes * max_numbers)
+        box = box_1 + box_2
+        boxes_list.append(box)
     m_top = rank_matrix >= (1 - lamda) * max_numbers
     m_bot = rank_matrix <= lamda * max_numbers
     m3 = m_top + m_bot
 
-    return m3
+    return m3, m_top, m_bot, boxes_list
 
 
 def get_matrices():
@@ -85,6 +99,19 @@ def get_matrices():
     dummy = dummy_matrix()
     CAP = CAP_matrix()
     return ret, dummy, CAP
+
+
+'''
+根据True-False矩阵计算组合收益率
+'''
+
+
+def computing_profolio_return_rate(m3: pd.DataFrame, ret: pd.DataFrame):
+    invest_numbers = m3.sum(axis=1)
+    result = (1 + ret) * m3  # 收益率乘以m3
+    mean_ret = result.sum(axis=1) / invest_numbers
+
+    return mean_ret
 
 
 '''
@@ -97,13 +124,23 @@ lamda：做多和做空比率
 '''
 
 
-def computing(ret: pd.DataFrame, dummy: pd.DataFrame, CAP: pd.DataFrame, lamda):
-    m3 = computing_1(CAP, dummy, lamda)  # 得到 True False 持仓矩阵
-    invest_numbers = m3.sum(axis=1)
-    result = (1 + ret) * m3  # 收益率乘以m3
-    mean_ret = result.sum(axis=1) / invest_numbers
-    total_ret = mean_ret.cumprod()
-    return m3, total_ret
+def computing(ret: pd.DataFrame, dummy: pd.DataFrame, CAP: pd.DataFrame, lamda, boxes):
+    m3, m_top, m_bot, boxes_list = computing_1(CAP, dummy, lamda, boxes)  # 得到 True False 持仓矩阵
+
+    # 计算收益率
+    total_ret = computing_profolio_return_rate(m3, ret)
+
+    # 计算不同boxes的收益率
+    ret_list = []
+    for box in boxes_list:
+        ret_box = computing_profolio_return_rate(box, ret)
+        ret_list.append(ret_box)
+
+    # 计算前分位数和后分位数头寸的收益
+    ret_top = computing_profolio_return_rate(m_top, ret)
+    ret_bot = computing_profolio_return_rate(m_bot, ret)
+
+    return m3, total_ret, ret_list, ret_top, ret_bot
 
 
 '''
@@ -121,9 +158,9 @@ def calculate_ic(factor: pd.DataFrame, ret: pd.DataFrame):
     a1 = (factor - factor_mean).fillna(value=0)
     a2 = (ret - ret_mean).fillna(value=0)
     a3 = a2.transpose()
-    matrix = np.dot(a1,a3)
+    matrix = np.dot(a1, a3)
     numbers = factor.count(axis=1)
-    cov = np.diagonal(matrix) / (numbers -1)
+    cov = np.diagonal(matrix) / (numbers - 1)
     std_factor = factor.std(axis=1)
     std_ret = ret.std(axis=1)
 
@@ -132,25 +169,76 @@ def calculate_ic(factor: pd.DataFrame, ret: pd.DataFrame):
     return ic
 
 
+# MAD:中位数去极值
+def filter_extreme_MAD(series, n=5):
+    median = series.quantile(0.5)
+    new_median = ((series - median).abs()).quantile(0.50)
+    max_range = median + n * new_median
+    min_range = median - n * new_median
+    return np.clip(series, min_range, max_range, axis=1)
+
+
+'''
+因子暴露展示
+输入：
+CAP：市值矩阵
+输出：
+valid数量变化折线图
+factor数值的分布图
+factor取极值之后的分布图
+'''
+
+
+def exposure(CAP: pd.DataFrame):
+    # 有效数字
+    valid_number = CAP.count(axis=1)
+    valid_number.plot()
+    plt.show()
+
+    # 直方图
+    dist = pd.DataFrame(CAP.to_numpy().flatten())
+    winsorize_plot(dist)
+
+    # 去极值后的直方图
+    mad_winsorize = filter_extreme_MAD(dist, 3)
+    winsorize_plot(mad_winsorize)
+    return None
+
+def mono_dist(ret_list):
+    # 计算加总
+    ret_cum_list=[]
+    for series in ret_list:
+        ret_cum_list.append(series.cumprod().tail(1))
+    return ret_cum_list
+
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
+    boxes = 3
+
     np.random.seed(3)
 
     # 生成三个矩阵(dataframe)：收益率，是否为指定成分股的dummy，最新市值
     ret, dummy, CAP = get_matrices()
 
     # 数组运算
-    portfolio, ret_total = computing(ret, dummy, CAP, 0.2)
+    portfolio, ret_total, ret_list, ret_top, ret_short = computing(ret, dummy, CAP, 0.2, 3)
 
     print("持仓矩阵：")
     print(portfolio)
 
     # 净值曲线展示
-    ret_total.plot()
+    ret_cum = ret_total.cumprod()
+    ret_cum.plot()
     plt.show()
 
     # 因子暴露
+    exposure(CAP)
+
+    # 单调性
     ic = calculate_ic(CAP[dummy].loc[:len(CAP) - 2, :], ret[dummy].loc[1:, :])
-    print(ic)
+    ic_cum = ic.cumsum()
+    mono_dist(ret_list)
+
+    #
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
